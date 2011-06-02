a496514ae1be378e16500acfd04c918686cba03c
Josh Haas <jphaas@gmail.com>
2011-06-02 18:10:36 -0400

    added session backup to local files

--- session.py
+++ session.py
@@ -1,11 +1,30 @@
-session = {}
+import tempfile
+from os.path import join, exists
+import pickle
+
+_backup = join(tempfile.gettempdir(), 'session_backup_file')
+if exists(_backup):
+    with open(_backup, 'r') as file:
+        try:
+            session = pickle.loads(file.read())
+        except EOFError:
+            print 'WARNING: session file corrupted'
+            session = {}
+else:
+    session = {}
+
+def back_up():
+    with open(_backup, 'w') as file:
+        file.write(pickle.dumps(session))
 
 def get_data(key):
     return session[key] if session.has_key(key) else None
 
 def set_data(key, value):
     session[key] = value
+    back_up()
  
 def clear_data(key):
     if session.has_key(key):
         del session[key]
+    back_up()
