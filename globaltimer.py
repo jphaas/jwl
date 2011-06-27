f1dd43f574e9f9ed2f4e5ed9ca01c85bf66415e5
Josh Haas <jphaas@gmail.com>
2011-06-27 12:29:18 -0400

    adding global timer

---
+++ globaltimer.py
@@ -0,0 +1,22 @@
+import time
+from jwl import deployconfig
+
+starttime = None
+buffer = None
+
+def start():
+    if not deployconfig.get('debug'): return
+    global starttime, buffer
+    starttime = time.time()
+    buffer = []
+
+def check(msg):
+    if not deployconfig.get('debug'): return
+    if starttime is not None:
+        buffer.append(msg + ': ' + str(time.time() - starttime))
+        
+def show_output():
+    if not deployconfig.get('debug'): return
+    check('in show output')
+    if buffer:
+        for l in buffer: print l
