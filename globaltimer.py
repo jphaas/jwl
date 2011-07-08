8b3c366093de69bb79e8f1e92d986b02f111b0a6
Josh Haas <jphaas@gmail.com>
2011-07-08 13:48:27 -0400

    make global timer give immediate feedback

--- globaltimer.py
+++ globaltimer.py
@@ -11,12 +11,15 @@ buffer = None
 def start():
     global starttime, buffer
     starttime = time.time()
-    buffer = []
+    # buffer = []
 
 def check(msg):
     if starttime is not None:
-        buffer.append(msg + ': ' + str(time.time() - starttime))
+        # buffer.append(msg + ': ' + str(time.time() - starttime))
+        logger.debug(msg + ': ' + str(time.time() - starttime))
         
 def show_output():
-    if buffer:
-        for l in buffer: logger.debug(l)
+    # if buffer:
+        # for l in buffer: logger.debug(l)
+    pass
+    
