1733f49ff6f796fc5104385347ed13da64c91a62
Josh Haas <jphaas@gmail.com>
2011-07-08 13:03:28 -0400

    globaltimer now works regardless of debug

--- globaltimer.py
+++ globaltimer.py
@@ -9,13 +9,11 @@ starttime = None
 buffer = None
 
 def start():
-    if not deployconfig.get('debug'): return
     global starttime, buffer
     starttime = time.time()
     buffer = []
 
 def check(msg):
-    if not deployconfig.get('debug'): return
     if starttime is not None:
         buffer.append(msg + ': ' + str(time.time() - starttime))
         
