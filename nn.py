d3f83747af5ceacf235c0ac08873ef782e2f61e2
Josh Haas <jphaas@gmail.com>
2011-08-05 22:19:49 -0400

    sql_lite store

---
+++ nn.py
@@ -0,0 +1,23 @@
+def setThreshold(node_id, threshold):
+    pass
+    
+def setConnection(from_node, to_node, multiplier):
+    pass
+    
+def getConnectedNodes(node_id): #all nodes where multiplier != 0, sorted in order of multiplier strength from greatest to least
+    pass
+    
+
+#can either just cycle through
+
+
+
+
+
+
+
+#cluster of nodes that represent changes.... maybe a couple of digit nodes to pick out the number
+
+#measure on correctness-divided-by-accuracy, or within time limit
+
+#evolve total number nodes and starting connections (gotta figure out how many nodes each node should be initially connected to, should put it in the middle so it is equally likely to be an and gate or an or gate)
---
+++ sqlite_store.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+def guess_type(obj):
+    types = {int: 'integer', long: 'integer', float: 'real', str: 'text', unicode: 'text'}
+    for k, v in types.iteritems():
+        if isinstance(obj, k) return v
+    return 'text'
+
+class Store:
+    def __init__(conn):
+        self.conn = conn
+        conn.row_factory = sqlite3.Row
+        cols = [r['name'] for r in conn.execute('pragma table_info(' + self.table + ')')]
+        self.cols = cols
+        if len(cols) == 0:
+            conn.execute('create table ' + self.table + '()')
+        
+    def insert(row):
+        for k, v in row.iteritems():
+            if k not in self.cols:
+                self.conn.execute('alter table add column ' + k + ' ' + guess_type(v))
+                self.cols.append(k)
+        self.conn.execute('insert into ' + self.table + '(%s) VALUES (%s)'%(', '.join(row.keys()), ', '.join(row.values())))
+
+        
+    table = 'data'
