8017e027e91b23c30b6d826ccfab1420c856deb6
Josh Haas <jphaas@gmail.com>
2011-08-09 10:05:41 -0400

    fixing

--- sqlite_store.py
+++ sqlite_store.py
@@ -5,6 +5,11 @@ def guess_type(obj):
     for k, v in types.iteritems():
         if isinstance(obj, k): return v
     return 'text'
+def clean_value(obj):
+    types = {int: 'integer', long: 'integer', float: 'real', str: 'text', unicode: 'text'}
+    for k, v in types.iteritems():
+        if isinstance(obj, k): return obj
+    return str(obj)
 
 class Store:
     def __init__(self, conn):
@@ -21,7 +26,7 @@ class Store:
                 self.conn.execute('alter table ' + self.table + ' add column "' + k + '" ' + guess_type(v))
                 self.cols.append(k)
         self.conn.execute('insert into ' + self.table + '(%s) VALUES (%s)'%(', '.join(['"' + k + '"' for k in row.keys()]), ', '.join(['?' for v in row.values()])),
-                row.values())
+                [clean_value(v) for v in row.values()])
 
     
         
