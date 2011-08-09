a6a95bd45264e451ecbdb16a919e81a3bab1f52a
Joshua Haas <jphaas@gmail.com>
2011-08-09 14:12:21 -0400

    fixes that I'm gonna merge over fuck

--- sqlite_store.py
+++ sqlite_store.py
@@ -1,10 +1,14 @@
 import sqlite3
 
+types = {int: 'integer', long: 'integer', float: 'real', str: 'text', unicode: 'text'}
 def guess_type(obj):
-    types = {int: 'integer', long: 'integer', float: 'real', str: 'text', unicode: 'text'}
     for k, v in types.iteritems():
         if isinstance(obj, k): return v
     return 'text'
+def clean_v(obj):
+    for k, v in types.iteritems():
+        if isinstance(obj, k): return obj
+    return str(obj)
 
 class Store:
     def __init__(self, conn):
@@ -21,7 +25,7 @@ class Store:
                 self.conn.execute('alter table ' + self.table + ' add column "' + k + '" ' + guess_type(v))
                 self.cols.append(k)
         self.conn.execute('insert into ' + self.table + '(%s) VALUES (%s)'%(', '.join(['"' + k + '"' for k in row.keys()]), ', '.join(['?' for v in row.values()])),
-                row.values())
+                [clean_v(v) for v in row.values()])
 
     
         
