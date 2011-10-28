59fcbc435404934b75f128b6a38ca9ea96618000
Joshua Haas <jphaas@gmail.com>
2011-10-28 16:37:53 -0400

    facilitating hot swap

---
+++ process_launcher.py
@@ -0,0 +1,42 @@
+#launches a child process.  When it gets a termination signal, passes that on to the child process.  Ends when the child process ends
+#python /home/ec2-user/keyword/code/launch_server.py
+
+import sys
+import subprocess
+import time
+import logging
+
+baselogger = logging.getLogger()
+std_out = logging.StreamHandler()
+baselogger.addHandler(std_out)
+baselogger.setLevel(level=logging.DEBUG)
+
+child_command = sys.argv[1]
+
+baselogger.info('starting subprocess....')
+
+p = subprocess.Popen(child_command.split(' '), stdin=None, stdout=None, stderr=None)
+
+done_now = False
+
+class EscapeException(Exception):
+    pass
+
+import signal
+def on_term(s, trace):
+    baselogger.info('got termination signal, terminating child')
+    p.send_signal(signal.SIGTERM)
+    baselogger.info('terminated child')
+    raise EscapeException()
+signal.signal(signal.SIGTERM, on_term)
+
+try:
+    while True:
+        s = p.poll()
+        if s: break
+        if done_now: break
+        time.sleep(0.01)
+except EscapeException:
+    pass
+    
+baselogger.info('and we are done')
--- tornado_launch.py
+++ tornado_launch.py
@@ -3,9 +3,24 @@ import tornado.ioloop
 import tornado.web
 import tornado.httpclient
 from .utils import curlpatch
+from tornado.httpserver import HTTPServer
+import time
 
 tornado.httpclient.AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
 
 def launch(application, port):
-    application.listen(port)
+    server = HTTPServer(application)
+    application._my_server = server
+    retries = 0
+    while True:
+        try:
+            server.listen(port, "")
+            break
+        except:
+            if retries < 40 / 0.01:
+                time.sleep(0.01)
+                retries += 1
+            else:
+                raise
+    print 'server started succesfully, starting ioloop'
     tornado.ioloop.IOLoop.instance().start()
